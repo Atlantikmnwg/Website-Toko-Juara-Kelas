4676da7d565106d550cbf111fcab88a719987c12
Atlantik Mannawang <barlan12312@gmail.com>
2025-07-26 19:43:59 +0700

    deploy

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanner("6b6668b1-c69e-40f1-9ac9-a5ccd2cb05b4");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produk Unggulan" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+
+const font = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Toko Juara Kelas",
+  description: "Website Toko Juara Kelas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
